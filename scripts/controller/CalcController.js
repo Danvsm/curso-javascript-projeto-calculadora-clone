e71c8f51deac3c719d65e4a053c95a2671df7c09
dan.vsm <dan.vsm@gmail.com>
2023-05-17 17:22:42 -0300

    cal

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,35 +1,66 @@
 class CalcController {
+
     constructor() {
-        this._currentDante;
+
+        this._locale = 'pt-BR'  
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
+        this._currentDate;
         this.initialize();
         
     }
 
     initialize() {
-        
+          this.setDisplayDateTime() 
+          setInterval(() =>{
+               setDisplayDateTime()   
+          }, 1000);
 
-        
-        this._dateEl.innerHTML = 'qqq'
-        this._timeEl.innerHTML = "ss"
     }
 
-   get displayCalc() {
-        return this._displayCalc.innerHTML;
-   }  
+     setDisplayDateTime() {
+          this.displayDate = this.currentDante.toLocaleDateString(this._locale, {
+             day: "2-digit",
+             month: "long",
+             year: "numeric"
 
-   set displayCalc(valor) {
-        this._displayCalc.innerHTML = valor;
+          });
+          this.displayTime = this.currentDante.toLocaleTimeString(this._locale);
+
+     }
+
+     get displayTime() {
+          this._timeEl.innerHTML
+
+     }
+
+     set displayTime(value) {
+          this._timeEl.innerHTML = value;
+
+     }
+
+     get displayDate() {
+          this._dateEl.innerHTML;
+     }
+   
+     set displayDate(value) {
+          this._dateEl.innerHTML = value;
+     }
+
+     get displayCalc() {
+          return this._displayCalcEl.innerHTML;
+     }  
+
+     set displayCalc(value) {
+          this._displayCalcEl.innerHTML = value;
     
-   }
+     }
 
-   get currentDante() {
-        return this._dataAtual;
-   }
 
-   set dataAtual(data) {
-        this._dataAtual = data;
-   }
+     get currentDante() {
+          return new Date();
+
+     }
+
 }
