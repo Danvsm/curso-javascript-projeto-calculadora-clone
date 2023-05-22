0380892537cd90317f2e6123a586d5569ed0a93e
dan.vsm <125106450+Danvsm@users.noreply.github.com>
2023-05-21 21:47:44 -0300

    up

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,7 +8,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
-        
+        this.initButtonsEvents()
     }
 
     initialize() {
@@ -16,13 +16,33 @@ class CalcController {
           
           setInterval( () => {
                this.setDisplayDateTime()
-          }, 1)
-                
-          initButtonsEvents(){
-               document.querySelectorAll("#buttons > g, #parts > g")
-          }
+          }, 1)     
 
     }
+
+    addEventListenerAll(element, events, fn) {
+          events.split(' ').forEach(event => {
+               element.addEventListener(event, fn)
+          })
+
+    }
+
+    
+    initButtonsEvents(){
+     let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+     buttons.forEach((btn, index) => {
+          this.addEventListenerAll(btn, 'click drag mouseover', e => {
+              
+               console.log(btn.className.baseVal.replace("btn-",""))
+              
+          });
+          
+          this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+               btn.style.cursor = "pointer"
+          });
+     });
+}
 
      setDisplayDateTime() {
           this.displayDate = this.currentDante.toLocaleDateString(this._locale, {
